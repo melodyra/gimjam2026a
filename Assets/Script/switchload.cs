45897b244283277c0617b49417aa2889775f8df0
BraveWind <bayupalamartawirawan@gmail.com>
2026-01-24 19:38:37 +0800

    Menghubungkan Menu Utama ke Setting
    Tambahan:
    1. Perlu tombol back
    2. Perlu update script untuk mengubah gameobject yang aktif
    3. Tambah credit + exit

---
+++ Assets/Script/switchload.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class swtichload : MonoBehaviour
+{
+    [SerializeField] GameObject input;
+    [SerializeField] GameObject output;
+    public void loading()
+    {
+        input.SetActive(false);
+        output.SetActive(true);
+    }
+}
