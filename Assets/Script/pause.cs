fa0c639d674cc57bd37069ea7fdacaf1615d80bc
BraveWind <bayupalamartawirawan@gmail.com>
2026-01-28 06:31:55 +0800

    Add Pause button

---
+++ Assets/Script/pause.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pause : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject[] lines1;
+    public GameObject Pause_Menu;
+    public void pausing()
+    {
+        Pause_Menu.SetActive(true);
+        for(int i = 0; i < lines1.Length; i++)
+        {
+            lines1[i].SetActive(false);
+        }
+    }
+}
